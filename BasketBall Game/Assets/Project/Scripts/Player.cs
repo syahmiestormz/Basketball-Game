abac0dde7d5139a89b214e7dd9d46e452a3cc54b
syahmiestormz <syahmie2008@gmail.com>
2017-08-14 15:44:38 +0800

    Ball
    Player script,ball holding

---
+++ BasketBall Game/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public GameObject ball;
+	public GameObject playerCamera;
+
+	public float ballDistance=2f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ball.GetComponent<Rigidbody> ().useGravity = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+	
+	}
+}
